421b958bfeadf1b79f677e14c77a9167848a171d
agungskak22 <agungskak22@gmail.com>
2019-04-01 23:56:53 +0700

    fix regist

--- js/login.js
+++ js/login.js
@@ -1,23 +1,5 @@
 var working = false;
 
-$('.Register').on('submit', function(e) {
-  e.preventDefault();
-  if (working) return;
-  working = true;
-  var $this = $(this),
-    $state = $this.find('button > .state');
-  $this.addClass('loading');
-  $state.html('Mengautentifikasi');
-  setTimeout(function() {
-    $this.addClass('ok');
-    $state.html('Cek Email Untuk Konfirmasi');
-    setTimeout(function() {
-      $state.html('Log in');
-      $this.removeClass('ok loading');
-      working = false;
-    }, 4000);
-  }, 3000);
-});
 $('.login').on('submit', function(e) {
   e.preventDefault();
   if (working) return;
